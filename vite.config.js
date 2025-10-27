ec8e8ed0b84e83c171f2514ef1a88a20fa626595
kobe-bayliss <kobebayliss1@gmail.com>
2025-10-27 16:53:05 +1300

    Moving root folder to /client

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: './client',
   base: '/',
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
+    emptyOutDir: true,
   },
 });
