209bb5f43d8a8925cb5cc17667e1bf75a77a95b3
kobe-bayliss <kobebayliss1@gmail.com>
2025-10-27 16:43:57 +1300

    Removing logos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import axios from 'axios'
 import { CopyButton } from './components/CopyButton'
 import './App.css'
